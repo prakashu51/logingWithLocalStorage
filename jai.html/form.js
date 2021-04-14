13a6efa39eded42ac01995b3c893cba8fe1ed711
prakashu51 <33785762+prakashu51@users.noreply.github.com>
2021-04-14 19:58:38 +0530

    final working version

--- jai.html/form.js
+++ jai.html/form.js
@@ -9,6 +9,25 @@ $(document).ready(function() {
 
 
 const addData = () => {
+    const firstName = $("#fname").val()
+    const lastName = $("#lname").val()
+    const gender = $("input[type='radio'][name='gender']:checked").val();
+    const age = $("#age").val();
+    const number = $("#mNumber").val();
+    const father = $("#fatherName").val();
+    const mother = $("#motherName").val();
+    const qualification = $("#quali").val();
+    const address = $("#address").val();
+    const state = $("#state").val();
+
+    const language = $("input[type='checkbox'][name='language']:checked").val();
+
+    const data = [firstName, lastName, gender, age, number, father, mother, qualification, address, state, language];
+
+
+
+    const jdata = JSON.stringify(data);
+    localStorage.setItem("userData", jdata);
 
     alert("form submitted successfully!")
 }
